d711c10d154021680b90ce77f6dda1e90c4599fa
ThomasArdal <thomasardal@gmail.com>
2013-02-16 22:00:31 +0100

    Fixed naming

--- HippoValidator.JSHintValidator/Properties/AssemblyInfo.cs
+++ HippoValidator.JSHintValidator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HippoValidator.JSHintValidator")]
+[assembly: AssemblyTitle("HippoValidator.JsHintValidator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HippoValidator")]
-[assembly: AssemblyProduct("HippoValidator.JSHintValidator")]
+[assembly: AssemblyProduct("HippoValidator.JsHintValidator")]
 [assembly: AssemblyCopyright("Copyright © HippoValidator 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
